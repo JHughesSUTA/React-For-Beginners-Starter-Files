d1049ac0e7a40eef372278f0782e98b18d0c5f82
James Hughes <james.a.hughes@gmail.com>
2018-06-18 15:29:11 -0500

    Vid 12 - handling events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,22 +4,14 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
   myInput = React.createRef();
 
-
-
-  // a 'construtor' method runs before teh component is created
-  // constructor() {
-  //   super();      // runs the thing we are extending first, needed in a constructor
-  //   this.goToStore = this.goToStore.bind(this);   // allows 'this' to be used in goToStore
-  // }
-
-  // goToStore(event) {      // 'event' argument not needed if we didn't use preventDefault();
   // use the syntax used one line below, or use a constructer commented out above
   goToStore = (event) => {
     // 1. stop form from submitting
     event.preventDefault();
     // 2. get the text from that input
-    console.log(this);
+    const storeName = this.myInput.current.value;    // tutorial used this.myInput.value.value ...
     // 3. change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
   }
 
   render() {
