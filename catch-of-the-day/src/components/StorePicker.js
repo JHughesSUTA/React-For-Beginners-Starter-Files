ba10a829b38530acbe093798b7a262e6a61f17e6
James Hughes <james.a.hughes@gmail.com>
2018-06-18 13:50:46 -0500

    Vid 11 - events, refs and 'this' binding

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,37 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+
+
+  // a 'construtor' method runs before teh component is created
+  // constructor() {
+  //   super();      // runs the thing we are extending first, needed in a constructor
+  //   this.goToStore = this.goToStore.bind(this);   // allows 'this' to be used in goToStore
+  // }
+
+  // goToStore(event) {      // 'event' argument not needed if we didn't use preventDefault();
+  // use the syntax used one line below, or use a constructer commented out above
+  goToStore = (event) => {
+    // 1. stop form from submitting
+    event.preventDefault();
+    // 2. get the text from that input
+    console.log(this);
+    // 3. change the page to /store/whatever-they-entered
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input 
+          type="text" 
+          ref={this.myInput}
+          required 
+          placeholder="Store Name" 
+          defaultValue={getFunName()} 
+        />
         <button type="submit">Visit Store -></button>
       </form>
     )
